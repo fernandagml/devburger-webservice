c68fb3e4080f1bfe6af36d952cdbb35549066e4d
Fernanda <fernandamarquesluizgaspar@gmail.com>
2026-04-09 16:22:48 -0300

    Correção de pesquisa no banco de dados e rota + Alteração no CSS.

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
 from flask import Flask, render_template as rt
-from model.produtos import recuperar_produtos as rp
+from model.produtos import recuperar_produtos as rp, recuperar_produto_id as rpid, recuperar_produto_destaque as rpd
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    produtos = rp(1)
-    return rt("index.html", produtos = produtos)
+    produtos = rp()
+    destaques = rpd()
+    return rt("index.html", produtos = produtos, destaques = destaques)
 
-@app.route("/hamburguer")
-def hamburguer():
-    return rt("/destaques.html")
+@app.route("/produto/<id>")
+def hamburguer(id):
+    produto = rpid(id)
+    return rt("/produto.html", produto = produto)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5050, debug=True)
--- model/produtos.py
+++ model/produtos.py
@@ -1,6 +1,6 @@
 from database.conexao import conectar
 
-def recuperar_produtos(estoque):
+def recuperar_produtos():
     """Função criada para buscar os produtos no banco de dados."""
 
     conexao, cursor = conectar()
@@ -8,3 +8,19 @@ def recuperar_produtos(estoque):
     produtos = cursor.fetchall()
     conexao.close()
     return produtos
+
+def recuperar_produto_id(id):
+
+    conexao, cursor = conectar()
+    cursor.execute("SELECT id_produto, nome_produto, preco, descricao, imagem, destaque, estoque FROM tb_produto WHERE id_produto = %s;", (id, ))
+    produto = cursor.fetchone()
+    conexao.close()
+    return produto
+
+def recuperar_produto_destaque():
+
+    conexao, cursor = conectar()
+    cursor.execute("SELECT id_produto, imagem, destaque, estoque FROM tb_produto WHERE destaque = 1;")
+    destaque = cursor.fetchall()
+    conexao.close()
+    return destaque
