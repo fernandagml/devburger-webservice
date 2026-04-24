d4b15e62eda031604f51c6865f2e2969f4125268
Fernanda <fernandamarquesluizgaspar@gmail.com>
2026-04-24 10:23:31 -0300

    Criação da tabela carrinho e itens-carrinho no banco de dados.

--- migrate/estrutura_db.sql
+++ migrate/estrutura_db.sql
@@ -15,3 +15,26 @@ CREATE TABLE IF NOT EXISTS tb_usuario (
     usuario VARCHAR(80) PRIMARY KEY,
     senha VARCHAR(200) NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS tb_carrinho (
+	id_carrinho INT PRIMARY KEY AUTO_INCREMENT,
+    data_carrinho DATE,
+    finalizado BOOL,
+    usuario VARCHAR(80),
+    CONSTRAINT FK_usuario_carrinho
+    FOREIGN KEY (usuario)
+    REFERENCES tb_usuario(usuario)
+);
+
+CREATE TABLE IF NOT EXISTS tb_itens_carrinho (
+	quantidade INT,
+    id_carrinho INT,
+	id_produto INT,
+    CONSTRAINT FK_produto_itens
+    FOREIGN KEY (id_produto)
+    REFERENCES tb_produto(id_produto),
+    CONSTRAINT FK_carrinho_itens
+    FOREIGN KEY (id_carrinho)
+    REFERENCES tb_caRrinho(id_carrinho),
+    PRIMARY KEY (id_carrinho, id_produto)
+);
